af667f42c77177fc6eb88f04d4e233abfa147c63
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-02-04 05:09:54 -0300

    adapting api into json

--- firstApi/index.js
+++ firstApi/index.js
@@ -2,7 +2,7 @@ const express = require('express') //importando modulos do express
 const app = express() //contem as funcionalidades do express
 
 app.get('/', (req, res) => {
-    res.send('Hello World! My name is Marlon')
+    res.json({mensagem:'Hello World! My name is Marlon'})
 }) //get recbe uma requisição do front e devolve uma resposta para o mesmo, o back se conecta com o front atraves do get
 
 app.listen(8080, () => {
