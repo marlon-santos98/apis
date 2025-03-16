67c7b583938c17ffa676786158023263c16daa13
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-03-16 07:44:44 -0300

    refactoring the code

--- api-mysql/index.js
+++ api-mysql/index.js
@@ -1,11 +1,36 @@
 import express from 'express'; // Usando import, já que o "type": "module" foi configurado
-import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
 const app = express();
 
 
 app.get('/campeonatos', async(req, res) => {
-    const campeonatos = await retornaCampeonatos()
-    res.json(campeonatos)
+    let campeonatos
+    const ano = req.query.ano
+    const time = req.query.time
+
+    if(typeof ano === 'undefined' && typeof time === 'undefined'){
+        campeonatos = await retornaCampeonatos()
+    } else if(typeof ano !== 'undefined') {
+        campeonatos = await retornaCampeonatosAno(parseInt(ano))
+    }  else if(typeof time !== 'undefined') {
+        campeonatos = await retornaCampeonatosTime(time)
+    }
+
+    if(campeonatos.length > 0){
+        res.json(campeonatos)
+    } else {
+        res.status(404).json({mensagem: 'Nenhum campeonato encontrado'})
+    }
+})
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const campeonato = await retornaCampeonatosID(id)
+    if(campeonato.length > 0){
+        res.json(campeonato)
+    } else {
+        res.status(404).json({mensagem: 'Nenhum campeonato encontrado'})
+    }
 })
 
 app.listen(9000, async () => {
--- api-mysql/servico/retornaCampeonatos_servico.js
+++ api-mysql/servico/retornaCampeonatos_servico.js
@@ -17,3 +17,19 @@ export async function retornaCampeonatosID(id) {
     conexao.release()
     return campeonatos
 }
+
+export async function retornaCampeonatosAno(ano) {
+    const conexao = await pool.getConnection()
+    const campeoantos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ' + ano)
+    const campeonatos = campeoantos_tb[0]
+    conexao.release()
+    return campeonatos
+}
+
+export async function retornaCampeonatosTime(time) {
+    const conexao = await pool.getConnection()
+    const campeoantos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao = "'+time+'"')
+    const campeonatos =  campeoantos_tb[0]
+    conexao.release()
+    return campeonatos
+}
