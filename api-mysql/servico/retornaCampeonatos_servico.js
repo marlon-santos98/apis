2cd3ebc0ccb135e405657f2a86856ce0d6711bde
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-03-17 05:15:40 -0300

    código refatorado

--- api-mysql/servico/retornaCampeonatos_servico.js
+++ api-mysql/servico/retornaCampeonatos_servico.js
@@ -1,10 +1,15 @@
 import pool from "./conexao.js";
 
+async function executaQuery(conexao, query) {
+    const resultado_query = await conexao.query(query)
+    const resposta = resultado_query[0]
+    return resposta
+}
+
 export async function retornaCampeonatos() {
     const conexao = await pool.getConnection()
-
-    const campeoantos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos')
-    const campeonatos = campeoantos_tb[0]
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos'
+    const campeonatos = executaQuery(conexao, query)
     conexao.release()
     return campeonatos
 }
@@ -12,24 +17,24 @@ export async function retornaCampeonatos() {
 export async function retornaCampeonatosID(id) {
     const conexao = await pool.getConnection()
 
-    const campeoantos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ' + id)
-    const campeonatos = campeoantos_tb[0]
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ' + id
+    const campeonatos = executaQuery(conexao, query)
     conexao.release()
     return campeonatos
 }
 
 export async function retornaCampeonatosAno(ano) {
     const conexao = await pool.getConnection()
-    const campeoantos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ' + ano)
-    const campeonatos = campeoantos_tb[0]
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ' + ano
+    const campeonatos = executaQuery(conexao, query)
     conexao.release()
     return campeonatos
 }
 
 export async function retornaCampeonatosTime(time) {
     const conexao = await pool.getConnection()
-    const campeoantos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao = "'+time+'"')
-    const campeonatos =  campeoantos_tb[0]
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao = "'+time+'"'
+    const campeonatos = executaQuery(conexao, query)
     conexao.release()
     return campeonatos
 }
